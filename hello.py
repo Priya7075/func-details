26098b1b9e02a0e1ecea06a549295c447af7eaf1
Priya7075 <102401793+Priya7075@users.noreply.github.com>
2022-04-26 12:54:41 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,4 @@
+def VI():
+    print('Welcome to python space')
+def students():
+    print("We are doing python programming")    
---
+++ signup-login.py
@@ -0,0 +1,11 @@
+print('------signup for creating an account ------')
+usr=input('enter username:')
+pwd=input('enter password: ')
+print('your account have been created succesfully----')
+print("login Now!")
+name=input('enter username: ')
+psw=input('enter password: ')
+if(usr==name and psw==pwd):
+    print('you have logged in succesfully!')
+else:
+    print('Invalid Details')    
